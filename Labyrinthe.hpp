5a5c2bf7ed35c345f629f6b23d0fd3d421220086
ArcSRa <as47du@gmail.com>
2023-05-27 19:29:54 +0200

    lab

--- Labyrinthe.hpp
+++ Labyrinthe.hpp
@@ -1,34 +1,55 @@
 #include "graph.hpp"
 
-//coins = [" ", "╶", "╵", "└", "╴", "─", "┘", "┴", "╷", "┌", "│", "├", "┐", "┬", "┤", "┼"]
-using namespace std;
+//coins = [" ", "╶", "╵", "└", "╴", "─", "┘", "┴", "╷", "┌", "│", "├", "┐", "┬", "┤", "┼"] //A voir pour les caractères qu'on utilisera
+static const vector<string> coins = { " ", "═", "║", "╚", "═", "═", "╝", "╩", "║", "╔", "║", "╠", "╗", "╦", "╣", "╬" };
 
-class Labyrinth : private Graphe{
+class Labyrinthe: public Graphe {
     private:
-        vector<vector<char>> repr;
-        vector<pair<int, int>> ouvertures;
-    public :
-    Labyrinth(int w = 0, int h = 0) : Graphe(w,h,false) {
-    
+    int w;
+    int h;
+    TupleMap A;
+    vector<vector<char>> repr;
+    vector<pair<int, int>> ouvertures;
+   
+    public:
+
+    Labyrinthe(int w = 0, int h = 0) : Graphe(w,h,false), w(w), h(h) {
+        A=Graphe::getGraph();
         this->reset();
-        //Tableau de la représentation du labyrinthe en mode semi-graphique
-        // for(int c; c<2*this->h+1;c++){
-        this->repr = vector<vector<char>>(2 * this->h + 1, vector<char>(2*this->w + 1, '*'));
-        // }
+              //Tableau de la représentation du labyrinthe en mode semi-graphique
+       // for(int c; c<2*this->h+1;c++){
+     vector<vector<char>> repr(2 * h + 1, vector<char>(2 * w + 1, '*'));
+
+
+       // }
         this->effacer_repr();
         //Coordonnées des ouvertures vers l’extérieur
-        this->ouvertures = vector<pair<int, int>>();
+       this->ouvertures = vector<pair<int, int>>();
+    } 
+    Labyrinthe(int w,int h,TupleMap list):Graphe(w,h,false), w(w), h(h),A(list){
+      this->reset();
+              //Tableau de la représentation du labyrinthe en mode semi-graphique
+       // for(int c; c<2*this->h+1;c++){
+     vector<vector<char>> repr(2 * h + 1, vector<char>(2 * w + 1, '*'));
+
+
+       // }
+        this->effacer_repr();
+        //Coordonnées des ouvertures vers l’extérieur
+       this->ouvertures = vector<pair<int, int>>();
+
     }
 
-    void reset() {   
-        this->A.clear();
-        for(int l=0;l<this->h;l++){
-            for(int c=0;c<this->w;c++){
-                this->ajouter_sommet((l,c));
-            }
-        }
-    } 
+    void reset()
+    {   
+        A.clear();
+    for(int l=0;l<this->h;l++){
+        for(int c=0;c<this->w;c++)
+            {tuple<int,int> coord(l,c);
+                this->ajouter_sommet(coord);}
 
+    }
+    }
     // void ToString(){
     //     this->construire_repr();
     //     string s;
@@ -36,9 +57,8 @@ class Labyrinth : private Graphe{
     //     {
     //         s.append(''+l);
     //     }
-    // }
-
-    friend ostream& operator<<(ostream& os,  Labyrinthe& labyrinthe) {
+    //}
+      friend ostream& operator<<(ostream& os,  Labyrinthe& labyrinthe) {
         labyrinthe.construire_repr();
         for (const auto& ligne : labyrinthe.repr) {
             for (const auto& cell : ligne) {
@@ -48,35 +68,77 @@ class Labyrinth : private Graphe{
         }
         return os;
     }
+   string construire_repr() {
+        string resultat;
+        // Les murs
+        for (int c = 0; c < this->w; c++) {
+            repr[0][2 * c + 1] = coins[5][0];
+        }
+        for (int l = 0; l < this->h; l++) {
+            repr[2 * l + 1][0] = coins[10][0];
+            for (int c = 0; c < this->w; c++) {
+                if (l + 1 < this->h && arete(make_pair(l, c), make_pair(l + 1, c))) {
+                    repr[2 * l + 2][2 * c + 1] = coins[0][0];
+                } else {
+                    repr[2 * l + 2][2 * c + 1] = coins[5][0];
+                }
 
-    void construire_repr() {//murs
-        int c,l;
-        for(c=0;c<this->w;c++) {
-            this->repr[0][2*c+1]=coins[5];
+                if (c + 1 < this->w && arete(make_pair(l, c), make_pair(l, c + 1))) {
+                    repr[2 * l + 1][2 * c + 2] = coins[0][0];
+                } else {
+                    repr[2 * l + 1][2 * c + 2] = coins[10][0];
+                }
+            }
         }
-        for(l=0;l<this->h;l++) {
-            this->repr[2*l+1][0]=coins[10];
+
+        // Les coins
+        for (long unsigned int l = 0; l < repr.size(); l += 2) {
+            for (long unsigned int c = 0; c < repr[0].size(); c += 2) {
+                int code = 1 * (c + 1 < repr[0].size() && repr[l][c + 1] != ' ');
+                code += 2 * ((l != 0) && (repr[l - 1][c] != ' '));
+                code += 4 * (c != 0 && repr[l][c - 1] != ' ');
+                code += 8 * (l + 1 < repr.size() && repr[l + 1][c] != ' ');
+                repr[l][c] = coins[code][0];
+            }
         }
-        for(c=0;c<this->w;c++) {
-            if l+1 < this->h and this.arete((l, c), (l+1, c)):
-                    self.repr[2*l+2][2*c+1] = Labyrinthe.coins[0]
-                else:
-                    self.repr[2*l+2][2*c+1] = Labyrinthe.coins[5]
-                if c+1 < self.w and self.arete((l, c), (l, c+1)):
-                    self.repr[2*l+1][2*c+2] = Labyrinthe.coins[0]
-                else:
-                    self.repr[2*l+1][2*c+2] = Labyrinthe.coins[10]
+
+        // Les ouvertures vers l'extérieur
+        for (const auto& o : ouvertures) {
+            int l = o.first;
+            int c = o.second;
+            if (c == 0) {
+                repr[2 * l + 1][2 * c] = ' ';
+            } else if (c == this->w - 1) {
+                repr[2 * l + 1][2 * c + 2] = ' ';
+            } else if (l == 0) {
+                repr[2 * l][2 * c + 1] = ' ';
+            } else if (l == this->h - 1) {
+                repr[2 * l + 2][2 * c + 1] = ' ';
+            }
         }
+
+        for (const auto& row : repr) {
+            for (char c : row) {
+                resultat += c;
+            }
+            resultat += '\n';
+        }
+
+        return resultat;
     }
 
-    void effacer_repr() {//const char vide=' ';
-        for(int l=0;l<this->h;l++) {
+    void effacer_repr()
+    {//const char vide=' ';
+        for(int l=0;l<this->h;l++)
+        {
             for (int c=0;c<this->w;c++){
                 this->repr[2*l+1][2*c+1] = ' ';
             }
         }
+
     }
 
+/*
     unordered_map<pair<int,int>,unordered_set<int>> dic_adjac(int w, int h) {
     }
     
@@ -90,11 +152,9 @@ class Labyrinth : private Graphe{
     unordered_set<int> voisins_cellule(int cell)
         
     void ouvrir_passage(x, y)
+    
         Graphe.ajouter_arete
-
-
-
-
+*/
 
 
 
--- main_proj.cpp
+++ main_proj.cpp
@@ -1,5 +1,5 @@
-#include "graph.hpp"
 
+#include "Labyrinthe.hpp"
 int main() {
 /*    Graphe g;
 
@@ -57,22 +57,44 @@ int main() {
     }
 */
 
-    Graphe g(5, 5, false); // Create a graph with width = 5, height = 5, and undirected edges
+    Graphe g(5, 4, false); // Create a graph with width = 5, height = 4, and undirected edges
 
     // Adding vertices
-    g.ajouter_sommet(make_tuple(0, 0));
-    g.ajouter_sommet(make_tuple(0, 1));
-    g.ajouter_sommet(make_tuple(1, 0));
-    g.ajouter_sommet(make_tuple(1, 1));
-    g.ajouter_sommet(make_tuple(2, 2));
+    // g.ajouter_sommet(make_tuple(0, 0));
+    // g.ajouter_sommet(make_tuple(0, 1));
+    // g.ajouter_sommet(make_tuple(1, 0));
+    // g.ajouter_sommet(make_tuple(1, 1));
+    // g.ajouter_sommet(make_tuple(2, 2));
 
-    // Adding edges
-    g.ajouter_arete(make_tuple(0, 0), make_tuple(0, 1));
-    g.ajouter_arete(make_tuple(0, 1), make_tuple(1, 1));
-    g.ajouter_arete(make_tuple(1, 0), make_tuple(1, 1));
-    g.ajouter_arete(make_tuple(0, 0), make_tuple(1, 0));
-    g.ajouter_arete(make_tuple(1, 1), make_tuple(2, 2));
-
+    // // Adding edges
+    // g.ajouter_arete(make_tuple(0, 0), make_tuple(0, 1));
+    // g.ajouter_arete(make_tuple(0, 1), make_tuple(1, 1));
+    // g.ajouter_arete(make_tuple(1, 0), make_tuple(1, 1));
+    // g.ajouter_arete(make_tuple(0, 0), make_tuple(1, 0));
+    // g.ajouter_arete(make_tuple(1, 1), make_tuple(2, 2));
+TupleMap lab1 = {
+    {make_tuple(0, 0), {make_tuple(0, 1), make_tuple(1, 0)}},
+    {make_tuple(0, 1), {make_tuple(0, 0), make_tuple(1, 1)}},
+    {make_tuple(0, 2), {make_tuple(1, 2), make_tuple(0, 3)}},
+    {make_tuple(0, 3), {make_tuple(0, 2), make_tuple(1, 3)}},
+    {make_tuple(1, 0), {make_tuple(0, 0), make_tuple(2, 0)}},
+    {make_tuple(1, 1), {make_tuple(0, 1), make_tuple(1, 2)}},
+    {make_tuple(1, 2), {make_tuple(1, 1), make_tuple(0, 2)}},
+    {make_tuple(1, 3), {make_tuple(0, 3), make_tuple(2, 3)}},
+    {make_tuple(2, 0), {make_tuple(1, 0), make_tuple(2, 1)}},
+    {make_tuple(2, 1), {make_tuple(2, 0)}},
+    {make_tuple(2, 2), {make_tuple(2, 3), make_tuple(3, 2)}},
+    {make_tuple(2, 3), {make_tuple(1, 3), make_tuple(2, 2), make_tuple(3, 3)}},
+    {make_tuple(3, 0), {make_tuple(3, 1), make_tuple(4, 0)}},
+    {make_tuple(3, 1), {make_tuple(3, 0), make_tuple(3, 2)}},
+    {make_tuple(3, 2), {make_tuple(3, 1), make_tuple(2, 2)}},
+    {make_tuple(3, 3), {make_tuple(2, 3)}},
+    {make_tuple(4, 0), {make_tuple(3, 0), make_tuple(4, 1)}},
+    {make_tuple(4, 1), {make_tuple(4, 0), make_tuple(4, 2)}},
+    {make_tuple(4, 2), {make_tuple(4, 1), make_tuple(4, 3)}},
+    {make_tuple(4, 3), {make_tuple(4, 2)}}
+};
+g.construire(lab1);
     // Get all vertices
     TupleMap graph = g.getGraph();
     cout << "Vertices:" << endl;
@@ -89,5 +111,8 @@ int main() {
         Tuple v = edge.second;
         cout << "(" << get<0>(u) << ", " << get<1>(u) << ") -> (" << get<0>(v) << ", " << get<1>(v) << ")" << endl;
     }
+    Labyrinthe test(5,4,lab1);
+     cout<<test<<endl;
+
     return 0;
 }
