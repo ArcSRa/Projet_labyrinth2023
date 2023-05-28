6ced3c4f45acb3e39ed4e73ac1a7c6afb36aa47d
G0lden0mega <bennani.othmani.othmane@gmail.com>
2023-05-28 10:48:28 +0200

    last one

--- graph.hpp
+++ graph.hpp
@@ -1,61 +1,53 @@
 #include <iostream>
-//#include <unordered_map>
 #include <set>
-#include <vector>
 #include <map>
+#include <vector>
 #include <algorithm>
-#include <cmath>
 
 
 using namespace std;
-using Tuple = tuple<int,int>;
-using TupleSet = set<Tuple>;
-using TupleMap = map<Tuple,TupleSet>;
-using Arete = pair<Tuple,Tuple>;
+using cell = pair<int,int>;
+using cellSet = set<cell>;
+using cellMap = map<cell,cellSet>;
+using Arete = pair<cell,cell>;
 
-//using TupleSet = unordered_set<Tuple>;
-//using TupleMap = unordered_map<Tuple, TupleSet>;
-//using DicoMap = unordered_map<Tuple, Tuple>;
+static constexpr cell None = make_pair(-1, -1);
 
 class Graphe {
 private:
-    TupleMap graph;
+    cellMap graph;
     int width;
     int height;
     bool oriented;
 
 public:
-    static constexpr Tuple None = make_tuple(-1, -1);
+    
     Graphe(int w, int h, bool oriented = true) : width(w), height(h), oriented(oriented) {}
 
-    TupleMap getGraph(){
+    cellMap getGraph(){
         return this->graph;
     }
 
-    void construire(TupleMap graph) {
+    void construire(cellMap graph) {
         this->graph=graph;
         this->height=0;
         this->width=0;
         int x,y;
-        for (TupleMap::iterator it = graph.begin(); it != graph.end(); it++) {
+        for (cellMap::iterator it = graph.begin(); it != graph.end(); it++) {
             x = get<0>(it->first);
             this->width = max(this->width, x + 1);
             y = get<1>(it->first);
             this->height = max(this->height, y + 1);
-            //const TupleVect& V = it->second;
-            //for (Tuple y : V) {
-            //    this->h = max(this->h, get<0>(y) + 1);
-            //}
         }
     }
 
-    void ajouter_sommet(Tuple x) {
+    void ajouter_sommet(cell x) {
         if (this->graph.find(x) == this->graph.end()) {
             this->graph[x] = {};
         }
     }
 
-    void ajouter_arete(Tuple x, Tuple y) {
+    void ajouter_arete(cell x, cell y) {
         this->ajouter_sommet(x);
         this->ajouter_sommet(y);
         this->graph[x].insert(y);
@@ -64,16 +56,16 @@ public:
         }
     }
 
-    TupleSet voisins(Tuple x) {
+    cellSet voisins(cell x) {
         return this->graph[x];
     }
 
     vector<Arete> aretes() {
         vector<Arete> L;
-        for (TupleMap::iterator it = this->graph.begin(); it != this->graph.end(); it++) {
-            Tuple x = it->first;
-            TupleSet V = it->second;
-            for (Tuple cell : V) {
+        for (cellMap::iterator it = this->graph.begin(); it != this->graph.end(); it++) {
+            cell x = it->first;
+            cellSet V = it->second;
+            for (cell cell : V) {
                 L.push_back(make_pair(x,cell));
             }
         }
@@ -81,35 +73,35 @@ public:
     }
     
     // Antécédent : les sommets x et y existent, sinon on met un assert
-    bool arete(Tuple x, Tuple y) {
+    bool arete(cell x, cell y) {
         return this->graph[y].find(x) != this->graph[y].end();
     }
 
-    TupleSet parcours_prof(Tuple s, TupleSet vus=TupleSet({None})) {
+    cellSet parcours_prof(cell s, cellSet vus=cellSet()) {
         if (vus.find(s) == vus.end()) {
             vus.insert(s);
-            for (Tuple v : this->voisins(s)) {
+            for (cell v : this->voisins(s)) {
                 parcours_prof(v, vus);
             }
         }
         return vus;
     }
 
-    TupleSet parcours_ch(Tuple s, Tuple ori=None, TupleSet vus=TupleSet({None})) {
+    cellSet parcours_ch(cell s, cell ori=None, cellSet vus=cellSet({None})) {
         if (vus.find(s) == vus.end()) {
             vus.insert(ori);
-            for (Tuple v : voisins(s)) {
+            for (cell v : voisins(s)) {
                 parcours_ch(v, s, vus);
             }  
         }
         return vus;
     }
 
-    TupleSet chemins(Tuple x, Tuple y) {
-        TupleSet vus = this->parcours_ch(x);
-        TupleSet c=TupleSet({});
+    cellSet chemins(cell x, cell y) {
+        cellSet vus = this->parcours_ch(x);
+        cellSet c=cellSet();
         if (vus.find(y) != vus.end()) {
-            for (TupleSet::iterator it=vus.begin();it!=vus.find(y);it++){
+            for (cellSet::iterator it=vus.begin();it!=vus.find(y);it++){
                 c.insert(*it);
             }
         }
@@ -117,19 +109,19 @@ public:
         return c;
     }
     
-    bool existe_chemin(Tuple x, Tuple y) {
-        TupleSet vus = this->parcours_prof(x);
+    bool existe_chemin(cell x, cell y) {
+        cellSet vus = this->parcours_prof(x);
         return vus.find(y) != vus.end();
     }
 
-    map<Tuple,int> parcours_larg(Tuple s) {
-        map<Tuple,int> dist = {{s,0}};
-        TupleSet cour = {s};
-        TupleSet suiv = TupleSet({});
+    map<cell,int> parcours_larg(cell s) {
+        map<cell,int> dist = {{s,0}};
+        cellSet cour = {s};
+        cellSet suiv = cellSet({});
         while (!cour.empty()) {
-            Tuple temp = *cour.begin();
+            cell temp = *cour.begin();
             cour.erase(s);
-            for (Tuple v : voisins(temp)) {
+            for (cell v : voisins(temp)) {
                 if (dist.find(v) == dist.end()) {
                     suiv.insert(v);
                     dist[v]=dist[s]+1;
@@ -137,20 +129,20 @@ public:
             }
             if (cour.empty()) {
                 cour=suiv;
-                suiv = TupleSet({});
+                suiv = cellSet({});
             }
         }
         return dist;
     }
 
-    TupleSet parcours_larg_chemin(Tuple s) {
-        TupleSet vus = TupleSet({None});
-        TupleSet cour = {s};
-        TupleSet suiv = TupleSet({});
+    cellSet parcours_larg_chemin(cell s) {
+        cellSet vus = cellSet({None});
+        cellSet cour = {s};
+        cellSet suiv = cellSet({});
         while (!cour.empty()) {
-            Tuple temp = *cour.begin();
+            cell temp = *cour.begin();
             cour.erase(s);
-            for (Tuple v : voisins(temp)) {
+            for (cell v : voisins(temp)) {
                 if (vus.find(v) == vus.end()) {
                     suiv.insert(v);
                     vus.insert(temp); // Warning
@@ -158,17 +150,17 @@ public:
             }
             if (cour.empty()) {
                 cour=suiv;
-                suiv = TupleSet({});
+                suiv = cellSet({});
             }
         }
         return vus;
     }
 
-    TupleSet chemins_larg(Tuple x, Tuple y) {
-        TupleSet vus = parcours_larg_chemin(x);
-        TupleSet c=TupleSet({});
+    cellSet chemins_larg(cell x, cell y) {
+        cellSet vus = parcours_larg_chemin(x);
+        cellSet c=cellSet({});
         if (vus.find(y) != vus.end()) {
-            for (TupleSet::iterator it=vus.begin();it!=vus.find(y);it++){
+            for (cellSet::iterator it=vus.begin();it!=vus.find(y);it++){
                 c.insert(*it);
             }
         }
@@ -176,8 +168,8 @@ public:
         return c;
     }
 
-    int distance(Tuple x, Tuple y) {
-        map<Tuple,int> dist = this->parcours_larg(x);
+    int distance(cell x, cell y) {
+        map<cell,int> dist = this->parcours_larg(x);
         return (dist.find(y)!=dist.end())?dist[y]:-1;
     }
 };
--- main_proj.cpp
+++ main_proj.cpp
@@ -57,7 +57,7 @@ int main() {
     }
 */
 
-    Graphe g(5, 5, false); // Create a graph with width = 5, height = 5, and undirected edges
+    /*Graphe g(5, 5, false); // Create a graph with width = 5, height = 5, and undirected edges
 
     // Adding vertices
     g.ajouter_sommet(make_tuple(0, 0));
@@ -88,6 +88,76 @@ int main() {
         Tuple u = edge.first;
         Tuple v = edge.second;
         cout << "(" << get<0>(u) << ", " << get<1>(u) << ") -> (" << get<0>(v) << ", " << get<1>(v) << ")" << endl;
+    }*/
+    // Create a TupleMap to represent the graph
+    cellMap graph = {
+        {make_pair(0,0),{make_pair(0,1),make_pair(1,0)}},
+        {make_pair(0,1),{make_pair(0,0),make_pair(1,1),make_pair(0,2)}},
+        {make_pair(0,2),{make_pair(0,1),make_pair(1,2)}},
+        {make_pair(1,0),{make_pair(0,0),make_pair(1,1)}},
+        {make_pair(1,1),{make_pair(1,0),make_pair(0,1),make_pair(1,2)}},
+        {make_pair(1,2),{make_pair(0,2),make_pair(1,1)}}
+    };
+
+    // Create an instance of the Graphe class
+    Graphe g(0, 0);
+
+    // Use the construire method to build the graph
+    g.construire(graph);
+
+    // Retrieve the built graph using getGraph method
+    cellMap retrievedGraph = g.getGraph();
+
+    // Print the retrieved graph
+    for (const auto& pair : retrievedGraph) {
+        const auto& cell = pair.first;
+        const auto& cellSet = pair.second;
+
+        cout << "(" << get<0>(cell) << ", " << get<1>(cell) << "): ";
+        for (const auto& value : cellSet) {
+            cout << "(" << get<0>(value) << ", " << get<1>(value) << ") ";
+        }
+        cout << endl;
     }
+
+    cellSet voisinsSet = g.voisins(make_pair(1, 1));
+    cout << "Voisins of (1, 1): ";
+    for (const auto& v : voisinsSet) {
+        cout << "(" << get<0>(v) << ", " << get<1>(v) << ") ";
+    }
+    cout << endl;
+
+    vector<Arete> aretesVec = g.aretes();
+    cout << "Aretes: ";
+    for (const auto& a : aretesVec) {
+        const auto& u = a.first;
+        const auto& v = a.second;
+        cout << "(" << get<0>(u) << ", " << get<1>(u) << ") - (" << get<0>(v) << ", " << get<1>(v) << ") ///";
+    }
+    cout << endl;
+
+    bool isArete = g.arete(make_pair(0, 0), make_pair(1, 2));
+    cout << "Is (0, 0) - (1, 2) an edge? " << (isArete ? "Yes" : "No") << endl;
+
+    cellSet parcoursProfSet = g.parcours_prof(make_pair(0, 0));
+    cout << "Parcours prof starting from (0, 0): ";
+    for (const auto& v : parcoursProfSet) {
+        cout << "(" << get<0>(v) << ", " << get<1>(v) << ") ";
+    }
+    cout << endl;
+
+    cellSet parcoursChSet = g.parcours_ch(make_pair(0, 0));
+    cout << "Parcours ch starting from (0, 0): ";
+    for (const auto& v : parcoursChSet) {
+        cout << "(" << get<0>(v) << ", " << get<1>(v) << ") ";
+    }
+    cout << endl;
+
+    cellSet cheminsSet = g.chemins(make_pair(0, 0), make_pair(2, 2));
+    cout << "Chemins from (0, 0) to (2, 2): ";
+    for (const auto& v : cheminsSet) {
+        cout << "(" << get<0>(v) << ", " << get<1>(v) << ") ";
+    }
+    cout << endl;
     return 0;
 }
